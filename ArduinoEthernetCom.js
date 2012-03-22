2e330093f9b6fe9b331ea6a67eb7d6933ae45e52
Sam Fentress <sfentress@concord.org>
2012-03-22 13:17:28 -0400

    First commit

---
+++ ArduinoEthernetCom.js
@@ -0,0 +1,111 @@
+/*
+  ==========================================================================
+  Project:   ArduinoEthernetCom
+  Copyright: ©2010 Concord Consortium, see README for license
+  Author:    Sam Fentress <sfentress@gmail.com>
+  ==========================================================================
+  
+  ArduinoEthernetCom library for simple communication with an Arduino via an Ethernet connection.
+  
+  See README for license and documentation, and example.html to see it live.
+  
+  This library will add two global objects to your JavaScript environment, ArduinoEthernetCom and the callback
+  function for the Arduino server, arduinoEthernetComCallback.
+*/
+  
+(function () {
+  
+  // Defaults
+  var ARDUINO_SERVER_IP    = "http://169.254.1.1",
+      FREQUENCY            = 2,
+      GENERATE_RANDOM_DATA = false;
+
+  var ArduinoEthernetCom = function (options) {
+      this.arduino_server_ip = options.arduino_server_ip || ARDUINO_SERVER_IP;
+      this.frequency = options.frequency || (options.delay_time ? 1000 / options.delay_time : 0 || FREQUENCY);
+      this.generate_random_data = options.generate_random_data || GENERATE_RANDOM_DATA;
+      
+      var self = this;
+      window.arduinoEthernetComCallback = function(jsonString) {
+        self.callback(jsonString);
+      }
+    };
+
+  // Private vars
+  var dataTimer,
+      observers      = [],
+      analogPins     = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5'];
+
+  ArduinoEthernetCom.prototype = {
+    addObserver: function (func) {
+      observers.push(func);
+    },
+
+    removeObserver: function (func) {
+      if (~observers.indexOf(func)) {
+        observers.splice(observers.indexOf(func), 1);
+      }
+    },
+
+    start: function () {
+      var self = this;
+      dataTimer = setInterval(function () {
+        self.getData();
+      }, (1000 / this.frequency));
+    },
+
+    stop: function () {
+      clearInterval(dataTimer);
+    },
+
+    getData: function () {
+      if (this.arduino_server_ip && !this.generate_random_data) {
+        this.getJSONP(this.arduino_server_ip, 'arduinoEthernetComCallback');
+      } else {
+        this.callback(this.nextRandomData());
+      }
+    },
+
+    currentRandomData: null,
+
+    nextRandomData: function () {
+      var i, ii, pin;
+      if (!this.currentRandomData) {
+        this.currentRandomData = {};
+        for (i = 0, ii = analogPins.length; i < ii; i++) {
+          pin = analogPins[i];
+          this.currentRandomData[pin] = Math.floor(Math.random() * 1025);
+        }
+      }
+
+      for (i = 0, ii = analogPins.length; i < ii; i++) {
+        pin = analogPins[i];
+        this.currentRandomData[pin] = this.currentRandomData[pin] + Math.floor(((Math.random() - 0.5) * 100));
+        this.currentRandomData[pin] = Math.max(Math.min(this.currentRandomData[pin], 1024), 0);
+      }
+
+      return JSON.stringify(this.currentRandomData);
+    },
+
+    callback: function (jsonString) {
+      var data = JSON.parse(jsonString),
+        i, ii;
+      for (i = 0, ii = observers.length; i < ii; i++) {
+        observers[i](data);
+      }
+    },
+    
+    getJSONP: function (url, callback) {
+      var oHead = document.getElementsByTagName('HEAD').item(0),
+          oScript= document.createElement("script");
+      oScript.type = "text/javascript";
+      oScript.src=url;
+      oHead.appendChild(oScript);
+      setTimeout(function() {
+          oHead.removeChild(oScript);
+      }, 1000);
+    }
+  };
+
+  window.ArduinoEthernetCom = ArduinoEthernetCom;
+})();
---
+++ README.md
@@ -0,0 +1,63 @@
+ArduinoEthernetCom: Simple communication between Arduinos and Browsers
+========================================================================
+
+The ArduinoEthernetCom library was created to simplify communication between Arduinos and browsers without requiring Flash or Java or drivers or anything else.
+
+ArduinoEthernetCom allows you to create a simple server on an Ethernet-enabled Arduino (i.e. an Arduino with an Ethernet shield, or an ArduinoEthernet) by uploading a tiny sketch to the device, and then stream data into the browser over an Ethernet connection.
+
+To read more about streaming data to your computer over an Ethernet cable, read [this blog post](http://blog.concord.org/streaming-arduino-data-to-a-browser).
+
+License
+-------
+
+Lab is Copyright 2012 (c) by the Concord Consortium and is distributed under
+any of the following licenses:
+
+- [Simplified BSD](http://www.opensource.org/licenses/BSD-2-Clause),
+- [MIT](http://www.opensource.org/licenses/MIT), or
+- [Apache 2.0](http://www.opensource.org/licenses/Apache-2.0).
+
+Using the library
+-----------------
+  
+    // Define an ArduinoEthernetCom with optional options
+    var arduinoEthernetCom = new ArduinoEthernetCom(_options_);
+    
+    // Define one or more observers to be notified each time the data updates
+    var dataDidUpdate = function(data) {
+      var pinA0Value = data.A0;           // data is returned as a hash of values
+      graph(pinA0Value)                   // do whatever
+    }
+  
+    arduinoEthernetCom.addObserver(dataDidUpdate);
+    
+    // start polling the Arduino, calling all the observers each time with data
+    arduinoEthernetCom.start();
+    
+    // some time later
+    arduinoEthernetCom.stop();
+    
+    // this will grab the current values of the pins. This is probably not as useful
+    // as the start() method, which continually polls the data.
+    arduinoEthernetCom.getData();
+    
+    
+### Options
+
+You can pass a hash of options to ArduinoEthernetCom, e.g. `var arduinoEthernetCom = new ArduinoEthernetCom({frequency: 2, generate_random_data: true});`
+
+* **arduino\_server\_ip**: 
+  * IP that the Arduino server is running on. **Default**: http://169.254.1.1
+* **frequency**: 
+  * Frequency of data collection in Hertz. **Default**: 4 (i.e. we will request data 4 times a second)
+* **delay\_time**: 
+  * Alternative way of defining frequency, in ms. **Default**: None, the above already defines 250 ms
+* **generate\_random\_data**: 
+  * If true, random floating voltages will be generated for all 6 pins. This means you don't need to connect an Arduino, so it can be used for testing. **Default**: false
+
+Using this with an Arduino on a web page
+----------------------------------------
+
+1. Upload the server sketch located at Arduino/JSONPSensorServer.ino to the Arduino. This will start a server running at http://169.254.1.1. If you need to use a different IP address, you must modify the sketch.
+2. Plug the Arduino into the computer with an Ethernet cable. Wait about 30 seconds for the server to boot up.
+3. Use the ArduinoEthernetCom library to get data from the Arduino.
